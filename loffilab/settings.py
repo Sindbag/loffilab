8c37a416fd1e9d7c3f7730707eb24e5bc9a4e000
Anatoly <sindbag@gmail.com>
2016-06-09 10:42:51 +0300

    Update settings.py

--- loffilab/settings.py
+++ loffilab/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@rav%oohpi2&euxlgrpjiqz^m(on&n+rv3ajf!7fhr)-82d5mo'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
